2108477b8cf8ee5e151077ca27c3e3b09b870607
주영빈 <tonty1222@naver.com>
2022-05-13 12:37:54 +0900

    Python 심화2

---
+++ ExDictionary.py
@@ -0,0 +1,41 @@
+# 본인의 정보를 적어주세요. "address" 키는 00시만 적어주세요. ex) 서울시
+Dic = {
+    "name" : "주영빈",
+    "age" : 22,
+    "phonenumber" : "01034205470",
+    "address" : "고양시"
+}
+
+# Key 값으로 리스트만들기 -- keys()
+
+
+# Value 값으로 리스트만들기 -- values()
+
+
+# Key 값 리스트로 변환하기 -- list
+
+
+# Value 값 리스트로 변환하기 -- list
+
+
+# Key 와 Value 쌍 얻기 -- items()
+
+
+# Key 로 Value 얻기 -- get()
+
+
+
+
+# 존재하지 않는 "grade" Key 가져오기 -- get()
+
+
+# default 값 "A" 설정하기 -- get()
+
+
+# Key 가 딕셔너리에 존재하는지 확인하기 -- in
+
+
+
+
+
+# Key 와 Value의 쌍을 모두 지우기 -- clear
